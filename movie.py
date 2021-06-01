e959d310314ed5d20f5099234c799dfe495ef9cd
safwanvk <safwanvalakundil@gmail.com>
2021-06-01 14:48:27 +0530

    :sparkles: Add get all movies

--- movie.py
+++ movie.py
@@ -1,18 +1,22 @@
-from flask import Response, request
+from flask import  jsonify, request,make_response
 from flask_restful import Resource
 from model import *
 
 class MoviesApi(Resource):
   def get(self):
-    movies = Movie.objects().to_json()
-    return Response(movies, mimetype="application/json", status=200)
+    try:
+      movies = db_operations.find()
+      data = [{'Name' : movie['name'], 'Casts' : movie['casts'], 'Genres': movie['genres']} for movie in movies]
+      return make_response(jsonify({'msg':'Success','data':data}), 200)
+    except Exception as e:
+      print(e)
+      return None
 
   def post(self):
     body = request.get_json()
     db_operations.insert_one(body)
+    return jsonify({'result' : 'Created successfully'}, 200)
 
-
-    return {'result' : 'Created successfully'}, 200
 
 class MovieApi(Resource):
   def put(self, id):
@@ -20,8 +24,8 @@ class MovieApi(Resource):
     Movie.objects.get(id=id).update(**body)
     return '', 200
 
-  def delete(self, id):
-    movie = Movie.objects.get(id=id).delete()
+  def delete(self):
+    db_operations.delete_one({'_id':ObjectId('60b5f1f2502eadbd05379293')})
     return '', 200
 
   def get(self, id):
