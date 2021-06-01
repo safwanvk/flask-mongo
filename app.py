618a907dec0f2e844457a8209e6f5eef09d194c3
safwanvk <safwanvalakundil@gmail.com>
2021-06-01 11:51:29 +0530

    :sparkles: Setup add movie

--- app.py
+++ app.py
@@ -10,7 +10,16 @@ app.config['MONGODB_SETTINGS'] = {
 
 initialize_db(app)
 
-
+@app.route('/movies', methods=['POST'])
+def add_movie():
+    try:
+        body = request.get_json()
+        movie = Movie(**body).save()
+        id = movie.id
+        return {'id': str(id)}, 200
+    except Exception as e:
+        print(e)
+        return {'msg':'error'}
 
 
 if __name__ == "__main__":
